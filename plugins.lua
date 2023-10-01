44c41fb49550243ee63a877ae75e13a439d26a95
William Bender <Will Bender>
2023-09-30 22:21:42 -0700

    Added action preview

--- plugins.lua
+++ plugins.lua
@@ -79,11 +79,32 @@ local plugins = {
     init = function ()
       require("bacon").setup({
         quickfix  = {
-         enabled = false, -- true to populate the quickfix list with bacon errors and warnings
-         event_trigger = true -- triggers the QuickFixCmdPost event after populating the quickfix list
+          enabled = false, -- true to populate the quickfix list with bacon errors and warnings
+          event_trigger = true -- triggers the QuickFixCmdPost event after populating the quickfix list
         }
       })
     end
+  },
+  {
+    "aznhe21/actions-preview.nvim",
+    init = function ()
+      require("actions-preview").setup {
+        telescope = {
+          sorting_strategy = "ascending",
+          layout_strategy = "vertical",
+          layout_config = {
+            width = 0.8,
+            height = 0.9,
+            prompt_position = "top",
+            preview_cutoff = 20,
+            preview_height = function(_, _, max_lines)
+              return max_lines - 15
+            end,
+          },
+        },
+      }
+    end
   }
 }
+
 return plugins
